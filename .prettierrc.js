ecc0e72dd018c2343f188ca18ae775c548efe227
Pupppppxz <63011078@kmitl.ac.th>
2023-08-06 15:14:55 +0700

    :hammer: implement prettier

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: true,
+  tabWidth: 4,
+  printWidth: 120,
+  singleQuote: true,
+  trailingComma: "all",
+  jsxSingleQuote: true,
+  bracketSpacing: true,
+  useTabs: false,
+};
